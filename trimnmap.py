229654d686b99f4854e60bb3aab81dc7ad9e893c
Aarsyth <39145409+tobyarnett@users.noreply.github.com>
2023-05-25 13:57:11 -0500

    Create trimnmap.py

---
+++ trimnmap.py
@@ -0,0 +1,62 @@
+####################################################################################
+#░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░NMAP Output File Trim░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░#
+#░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░Developed by Aarsyth░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░#
+#░░░░░░░░░░░░GitHub Repository: https://github.com/CyberOneTechnologies/░░░░░░░░░░░#
+#░░░░░░░░░░░░░░░░░For support, reach out on Discord: Aarsyth#0563░░░░░░░░░░░░░░░░░░#
+####################################################################################
+#░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░#
+#░░░░░░░░█████╗░██╗░░░██╗██████╗░███████╗██████╗░░█████╗░███╗░░██╗███████╗░░░░░░░░░#
+#░░░░░░░██╔══██╗╚██╗░██╔╝██╔══██╗██╔════╝██╔══██╗██╔══██╗████╗░██║██╔════╝░░░░░░░░░#
+#░░░░░░░██║░░╚═╝░╚████╔╝░██████╦╝█████╗░░██████╔╝██║░░██║██╔██╗██║█████╗░░░░░░░░░░░#
+#░░░░░░░██║░░██╗░░╚██╔╝░░██╔══██╗██╔══╝░░██╔══██╗██║░░██║██║╚████║██╔══╝░░░░░░░░░░░#
+#░░░░░░░╚█████╔╝░░░██║░░░██████╦╝███████╗██║░░██║╚█████╔╝██║░╚███║███████╗░░░░░░░░░#
+#░░░░░░░░╚════╝░░░░╚═╝░░░╚═════╝░╚══════╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚══╝╚══════╝░░░░░░░░░#
+#░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░#
+####################################################################################
+#░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░#
+####################################################################################
+#░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░Description:░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░#
+#----------------------------------------------------------------------------------#
+#░The Python script provided is designed to trim the output of an Nmap scan. It░░░░#
+#░removes downed hosts and time stamp messages from the input file, improving the░░# 
+#░readability of the scan results. The user is prompted to provide the paths for░░░# 
+#░the input and output files. The script efficiently processes the input file,░░░░░# 
+#░filters out irrelevant information, and writes the trimmed output to the░░░░░░░░░#
+#░specified location. It streamlines the analysis of Nmap scan results, making it░░# 
+#░easier to extract meaningful insights from the data.░░░░░░░░░░░░░░░░░░░░░░░░░░░░░#
+#░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░#
+#░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░#
+#░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░#
+####################################################################################
+
+
+def trim_nmap_output():
+    input_file = input("Enter the path of the input file to trim: ")
+    output_file = input("Enter the path and name of the output file for the trimmed version: ")
+
+    with open(input_file, 'r') as f:
+        lines = f.readlines()
+
+    trimmed_lines = []
+    is_host_section = False
+
+    for line in lines:
+        line = line.strip()
+
+        # Check if it is a host section
+        if line.startswith("Nmap scan report for"):
+            is_host_section = True
+        elif line.startswith("Host is up") or line.startswith("Note: Host seems down"):
+            is_host_section = False
+
+        # Add line if it is not a host section, and not a time stamp message
+        if not is_host_section and not line.startswith("Nmap done:"):
+            trimmed_lines.append(line)
+
+    with open(output_file, 'w') as f:
+        f.write('\n'.join(trimmed_lines))
+
+    print("Trimmed output written to", output_file)
+
+# Usage
+trim_nmap_output()
